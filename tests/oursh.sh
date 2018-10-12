8a4952c5b3821679cc7247397c7f6d577821f006
Nathan Lilienthal <nathan@nixpulvis.com>
2018-10-11 22:53:29 -0400

    Add two basic oursh tests.

---
+++ tests/oursh.sh
@@ -0,0 +1,47 @@
+use std::io::Write;
+use std::process::{Command, Stdio};
+
+#[test]
+fn hello_world() {
+    let mut child = Command::new("target/debug/oursh")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("error swawning oursh process");
+
+    {
+        let stdin = child.stdin.as_mut()
+            .expect("error opening stdin");
+        stdin.write_all("echo hello world".as_bytes())
+            .expect("error writing to stdin");
+    }
+
+    let output = child.wait_with_output()
+        .expect("error reading stdout");
+
+    assert_eq!("hello world\n", String::from_utf8_lossy(&output.stdout));
+    assert!(output.stderr.is_empty());
+}
+
+#[test]
+#[should_panic]
+fn hello_world_quoted() {
+    let mut child = Command::new("target/debug/oursh")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("error swawning oursh process");
+
+    {
+        let stdin = child.stdin.as_mut()
+            .expect("error opening stdin");
+        stdin.write_all("echo \"hello world\"".as_bytes())
+            .expect("error writing to stdin");
+    }
+
+    let output = child.wait_with_output()
+        .expect("error reading stdout");
+
+    assert_eq!("hello world\n", String::from_utf8_lossy(&output.stdout));
+}
