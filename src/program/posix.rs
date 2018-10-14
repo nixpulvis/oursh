5304f27b36d1b5681a1dc6c0e8156e9512bd32c9
Nathan Lilienthal <nathan@nixpulvis.com>
2018-10-14 17:57:36 -0400

    Remove cruft.

--- src/program/posix.rs
+++ src/program/posix.rs
@@ -153,12 +153,6 @@ impl super::Command for Command {
                 }).collect();
                 Job::new(argv).run();
             },
-            // Command::Pair(ref left, ref right) => {
-            //     left.run()
-            //         .expect("error running command");
-            //     right.run()
-            //         .expect("error running command");
-            // },
             Command::Compound(ref program) => {
                 for command in program.0.iter() {
                     command.run()
