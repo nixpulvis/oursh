dd9b8c9b1764fe465741aa89fb8651128fbc9c2f
Nathan Lilienthal <nathan@nixpulvis.com>
2018-10-14 17:01:54 -0400

    Start real arg parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,87 @@
 #[macro_use]
 extern crate oursh;
 extern crate termion;
+extern crate docopt;
 
 use std::env;
 use std::io::{self, Read};
+use std::fs::File;
 use oursh::program::{parse_primary, Program};
 use oursh::repl;
 use termion::is_tty;
+use docopt::{Docopt, ArgvMap, Value};
+
+// Write the Docopt usage string.
+const USAGE: &'static str = "
+Usage: oursh [-#] [<file>]
+
+Options:
+    -#, --ast  Print an AST of each program.
+";
 
 // Our shell, for the greater good. Ready and waiting.
 fn main() {
-    // Standard input file descriptor (0), used for user input from the
-    // user of the shell.
-    let stdin = io::stdin();
+    // Parse argv and exit the program with an error message if it fails.
+    let args = Docopt::new(USAGE)
+                      .and_then(|d| d.argv(env::args().into_iter()).parse())
+                      .unwrap_or_else(|e| e.exit());
 
-    // Process text in raw mode style if we're attached to a tty.
-    if is_tty(&stdin) {
-        // Standard output file descriptor (1), used to display program output
-        // to the user of the shell.
-        let stdout = io::stdout();
+    if let Some(Value::Plain(Some(ref filename))) = args.find("<file>") {
+        let mut file = File::open(filename)
+            .expect(&format!("error opening file: {}", filename));
 
-        // Start a program running repl.
-        repl::start(stdin, stdout, parse_and_run);
-    } else {
         // Fill a string buffer from STDIN.
-        let mut text = String::new();
-        stdin.lock().read_to_string(&mut text).unwrap();
+        let mut text= String::new();
+        file.read_to_string(&mut text)
+            .expect("error reading file");
 
         // Run the program.
-        parse_and_run(&text);
+        parse_and_run(&args)(&text);
+    } else {
+        // Standard input file descriptor (0), used for user input from the
+        // user of the shell.
+        let stdin = io::stdin();
+
+        // Process text in raw mode style if we're attached to a tty.
+        if is_tty(&stdin) {
+            // Standard output file descriptor (1), used to display program output
+            // to the user of the shell.
+            let stdout = io::stdout();
+
+            // Start a program running repl.
+            repl::start(stdin, stdout, parse_and_run(&args));
+        } else {
+            // Fill a string buffer from STDIN.
+            let mut text = String::new();
+            stdin.lock().read_to_string(&mut text).unwrap();
+
+            // Run the program.
+            parse_and_run(&args)(&text);
+        }
     }
 }
 
-fn parse_and_run(text: &String) {
-    // Parse with the primary grammar and run each command in order.
-    match parse_primary(text.as_bytes()) {
-        Ok(program) => {
-            // TODO #9: Proper arg parsing.
-            if let Some(arg1) = env::args().nth(1) {
-                if arg1 == "-#" || arg1 == "--ast" {
-                    debug!(program);
-                }
-            }
+enum RunMode {
+    Repl,
+    Stdin,
+    File,
+}
 
-            program.run()
-                .expect(&format!("error running program: {:?}", program));
-        },
-        Err(()) => {
-            println!("error parsing text: {}", text);
+// fn ::
+
+fn parse_and_run<'a>(args: &'a ArgvMap) -> impl Fn(&String) + 'a {
+    move |text: &String| {
+        // Parse with the primary grammar and run each command in order.
+        match parse_primary(text.as_bytes()) {
+            Ok(program) => {
+                if args.get_bool("-#") { debug!(program); }
+
+                program.run()
+                    .expect(&format!("error running program: {:?}", program));
+            },
+            Err(()) => {
+                println!("error parsing text: {}", text);
+            }
         }
     }
 }
