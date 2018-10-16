6c888b8354541e83403388cf23099ca6d4b0b72a
Nathan Lilienthal <nathan@nixpulvis.com>
2018-10-16 05:18:21 +0000

    rebuild pages at 71a4e0d

--- oursh/repl/completion/sidebar-items.js
+++ oursh/repl/completion/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["complete",""],["executable_complete",""]]});
+initSidebarItems({"enum":[["Completion",""]],"fn":[["complete","Return a completed (valid) program text from the partial string given."],["executable_completions","Return a list of the matches from the given partial program text."],["path_complete","Complete a path at the end of the given string."]]});
