4c2bb96b2628a22b2b053a3bf5da8b6eb048ada5
Nathan Lilienthal <nathan@nixpulvis.com>
2018-10-21 15:17:35 -0400

    hello_world test is working again.

--- src/program/posix/lex.rs
+++ src/program/posix/lex.rs
@@ -3,6 +3,7 @@ use std::str::{self, CharIndices};
 pub type Spanned<T, E> = Result<(usize, T, usize), E>;
 pub type Span<'input> = Spanned<Tok<'input>, Error>;
 
+#[derive(Debug)]
 pub struct Error;
 
 #[derive(Debug)]
@@ -20,7 +21,8 @@ pub enum Tok<'input> {
     Pipe,
     Dollar,
     Equals,
-    Backslack,
+    Slash,
+    Backslash,
     DoubleQuote,
     SingleQuote,
     RCaret,
@@ -45,7 +47,7 @@ pub struct Lexer<'input> {
 
 impl<'input> Lexer<'input> {
     pub fn new(input: &'input str) -> Self {
-        let chars = input.char_indices();
+        let mut chars = input.char_indices();
         let lookahead = chars.next();
         Lexer { input, chars, lookahead }
     }
@@ -54,20 +56,71 @@ impl<'input> Lexer<'input> {
 impl<'input> Lexer<'input> {
     pub fn advance(&mut self) -> Option<(usize, char)> {
         match self.lookahead {
-            Some(t) => {
-                self.lookahead = self.chars.next().map(|(i,c)| {
-                    (i, c)
-                });
-                Some(t)
+            Some((i, t)) => {
+                self.lookahead = self.chars.next();
+                Some((i, t))
             },
             None => None,
         }
     }
 
-    fn word(&mut self, start: usize) -> Result<(usize, Tok, usize), Error> {
-        let end = start + 3;
-        Ok((start, Tok::Word(self.input[start..end]), end))
+    pub fn take_until<F>(&mut self, start: usize, mut terminate: F) -> (usize, &'input str)
+    where
+        F: FnMut(char) -> bool,
+    {
+        let mut end = start + 1;
+        while let Some((e, c)) = self.lookahead {
+            if terminate(c) {
+                return (e, &self.input[start..end]);
+            } else {
+                end = e + 1;
+                self.advance();
+            }
+        }
+        (end, &self.input[start..end])
     }
+
+    fn take_while<F>(&mut self, start: usize, mut keep_going: F) -> (usize, &'input str)
+    where
+        F: FnMut(char) -> bool,
+    {
+        self.take_until(start, |c| !keep_going(c))
+    }
+
+    pub fn word(&mut self, start: usize) -> Result<(usize, Tok<'input>, usize), Error> {
+        let (mut end, mut ident) = self.take_while(start, is_ident_continue);
+        Ok((start, Tok::Word(&self.input[start..end]), end))
+    }
+
+    pub fn shebang_block(&mut self, start: usize) -> Option<(usize, Tok<'input>, usize)> {
+        let (end, line) = self.take_until(start, |ch| ch == '\n');
+
+        if line.starts_with("{#!") {
+            Some((start, Tok::Shebang, end))
+        } else {
+            None
+        }
+    }
+}
+
+fn is_ident_start(ch: char) -> bool {
+    // TODO: Unicode?
+    match ch {
+        '-' | '_' | '.' | 'a'...'z' | 'A'...'Z' => true,
+        _ => false,
+    }
+}
+
+fn is_ident_continue(ch: char) -> bool {
+    // TODO: Unicode?
+    match ch {
+        '0'...'9' | '\'' => true,
+        ch => is_ident_start(ch),
+    }
+}
+
+fn is_whitespace(ch: char) -> bool {
+    ch == ' ' || ch == '\t'
 }
 
 impl<'input> Iterator for Lexer<'input> {
@@ -75,105 +128,42 @@ impl<'input> Iterator for Lexer<'input> {
 
     fn next(&mut self) -> Option<Self::Item> {
         while let Some((i, c)) = self.advance() {
-            match c {
-                ' ' => continue,
-                c1 => {
-                    return Some(self.word(i));
-                }
-            }
-        }
-
-        None
-    }
-}
-//         {
-//             let peek = self.chars.peek();
-//             println!("top peek: {:?}", peek);
-//             if let None = peek {
-//                 println!("was none");
-//                 return None;
-//             }
-//         }
-
-//         loop {
-//             match self.chars.next() {
-//                 // Some((i, '#'))  => self.start_comment(),
-//                 Some((i, ' ')) => {
-//                     println!("emit: Space");
-//                     return Some(Ok((i, Tok::Space, i+1)));
-//                 },
-//                 Some((i, '\t')) => {
-//                     println!("emit: Tab");
-//                     return Some(Ok((i, Tok::Tab, i+1)));
-//                 },
-//                 Some((i, '\n')) => {
-//                     println!("emit: Linefeed");
-//                     return Some(Ok((i, Tok::Linefeed, i+1)));
-//                 },
-//                 Some((i, c)) => {
-//                     println!("top: {:?}", c);
-//                     let mut k = i+1;
-
-//                     if let None = self.chars.peek() {
-//                         let tok = Tok::Word(&self.input[i..k]);
-//                         println!("emit {:?}", tok);
-//                         return Some(Ok((i, tok, k)));
-//                     }
-
-//                     loop {
-//                         match self.chars.next() {
-//                             // Some((i, '#'))  => self.start_comment(),
-//                             Some((j, ' '))  |
-//                             Some((j, '\t')) |
-//                             Some((j, '\n')) => {
-//                                 println!("word: break whitespace");
-//                                 let tok = Tok::Word(&self.input[i..k]);
-//                                 println!("emit {:?}", tok);
-//                                 return Some(Ok((i, tok, k)));
-//                             },
-//                             Some((j, c)) => {
-//                                 println!("word: {:?}", c);
-//                                 k += 1;
-//                             },
-//                             None => {
-//                                 println!("word: break EOF");
-//                                 let tok = Tok::Word(&self.input[i..k]);
-//                                 println!("emit {:?}", tok);
-//                                 return Some(Ok((i, tok, k)));
-//                             }
-//                         }
-//                     }
-//                 },
-//                 None => return None,
-//             }
-//         }
-    // }
-// }
-
-impl<'input> Lexer<'input> {
-    fn lex_word<'a: 'input>(&mut self,
-                            i: usize,
-                            l: char,
-                            mut word: &'a mut [u8])
-        -> Option<Span<'input>>
-    {
-        let mut j = i;
-        loop {
-            let next = self.chars.next();
-            println!("{:?}", next);
-            match next {
-                // Some((i, '#'))  => self.start_comment(),
-                Some((i, ' '))  |
-                Some((i, '\t')) |
-                Some((i, '\n')) => break,
-                None => break,
-                Some((i, c)) => {
-                    println!("{}, {}", i, j);
-                    word[j] = c as u8;
-                    j = i;
+            return match c {
+                '\n' => Some(Ok((i, Tok::Linefeed, i+1))),
+                // TODO: And,
+                '&'  => Some(Ok((i, Tok::Amper, i+1))),
+                '}'  => Some(Ok((i, Tok::RBrace, i+1))),
+                '{'  => Some(Ok((i, Tok::LBrace, i+1))),
+                ')'  => Some(Ok((i, Tok::RParen, i+1))),
+                '('  => Some(Ok((i, Tok::LParen, i+1))),
+                '`'  => Some(Ok((i, Tok::Backtick, i+1))),
+                '!'  => Some(Ok((i, Tok::Bang, i+1))),
+                // TODO: Or,
+                '|'  => Some(Ok((i, Tok::Pipe, i+1))),
+                '$'  => Some(Ok((i, Tok::Dollar, i+1))),
+                '='  => Some(Ok((i, Tok::Equals, i+1))),
+                '/'  => Some(Ok((i, Tok::Slash, i+1))),
+                '\\' => Some(Ok((i, Tok::Backslash, i+1))),
+                '"'  => Some(Ok((i, Tok::DoubleQuote, i+1))),
+                '\'' => Some(Ok((i, Tok::DoubleQuote, i+1))),
+                '>'  => Some(Ok((i, Tok::RCaret, i+1))),
+                '<'  => Some(Ok((i, Tok::LCaret, i+1))),
+                '{' => {
+                    match self.shebang_block(i) {
+                        Some(token) => Some(Ok(token)),
+                        None => continue,
+                    }
                 },
+                c if is_ident_start(c) => {
+                    let tok = self.word(i);
+                    Some(tok)
+                },
+                c if is_whitespace(c) => {
+                    Some(Ok((i, Tok::Space, i+1)))
+                },
+                c => panic!("unexpected char {:?}", c),
             }
         }
-        Some(Ok((i, Tok::Word(str::from_utf8(&word[i..j]).unwrap()), j)))
+        None
     }
 }
