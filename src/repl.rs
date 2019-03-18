65ae9791d287a1977c0c2afcade7ddfb1d05c8bb
Nathan Lilienthal <nathan@nixpulvis.com>
2019-03-18 19:38:04 -0400

    Move prompt into module.

--- src/repl.rs
+++ src/repl.rs
@@ -3,12 +3,10 @@
 //! There will be *absolutely no* blocking STDIN/OUT/ERR on things like tab
 //! completion or other potentially slow, or user defined behavior.
 
-use std::io::{Write, Stdin, Stdout};
-use nix::unistd;
-use pwd::Passwd;
-use termion::{style, color};
+use std::io::{Stdin, Stdout};
 use crate::program::Result;
 use self::actions::Action;
+use self::prompt::Prompt;
 
 #[cfg(feature = "raw")]
 use {
@@ -118,85 +116,9 @@ pub fn start<F>(mut stdin: Stdin, mut stdout: Stdout, runner: F)
     }
 }
 
-/// A status prompt to be displayed in interactive sessions before each
-/// program.
-pub struct Prompt(String);
-
-impl Prompt {
-    /// The most basic possible prompt.
-    pub const DEFAULT_FORMAT: &'static str = "$ ";
-
-    pub fn new() -> Self {
-        Prompt(format!("{}", Self::DEFAULT_FORMAT))
-    }
-
-    pub fn sh_style(self) -> Self {
-        const NAME: &'static str = "oursh";
-        const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-        let version = &VERSION[0..(VERSION.len() - 2)];
-        // TODO: Add a flag for pretending to be `sh`.
-        // let name = "sh";
-        // let version = "4.4";
-        Prompt(format!("{}-{}$ ", NAME, version))
-    }
-
-    pub fn nixpulvis_style(self) -> Self {
-        let mut buf = [0u8; 64];
-        let hostname_cstr = unistd::gethostname(&mut buf)
-            .expect("error getting hostname");
-        let hostname = hostname_cstr.to_str()
-            .expect("hostname wasn't valid UTF-8");
-        let passwd = Passwd::current_user()
-            .expect("error i don't exist, passwd validation failed!");
-        let whoami = passwd.name;
-        let cwd = unistd::getcwd()
-            .expect("error reading cwd");
-        Prompt(format!("{}{}{}@{}{}{}:{}{}{}{}$ ",
-            color::Fg(color::Red),
-            whoami,
-            color::Fg(color::Reset),
-            color::Fg(color::Blue),
-            hostname,
-            color::Fg(color::Reset),
-            color::Fg(color::Green),
-            cwd.display(),
-            color::Fg(color::Reset),
-            " "))
-    }
-
-    pub fn long_style(self) -> Self {
-        let mut buf = [0u8; 64];
-        let hostname_cstr = unistd::gethostname(&mut buf)
-            .expect("error getting hostname");
-        let hostname = hostname_cstr.to_str()
-            .expect("hostname wasn't valid UTF-8");
-        Prompt(format!("{}{} {} $ {} {} {} {} {}{} ",
-            style::Invert,
-            color::Fg(color::Green),
-            hostname,
-            color::Fg(color::Yellow),
-            color::Fg(color::Red),
-            color::Fg(color::Magenta),
-            color::Fg(color::Cyan),
-            color::Fg(color::Reset),
-            style::Reset))
-    }
-
-    pub fn short_style(self) -> Self {
-        Prompt(format!("{}{}our$h{}{} ",
-            color::Fg(color::Red),
-            style::Invert,
-            color::Fg(color::Reset),
-            style::Reset))
-    }
-
-    pub fn display(&self, stdout: &mut impl Write) {
-        write!(stdout, "{}", self.0).unwrap();
-        stdout.flush().unwrap();
-    }
-}
 
 pub mod actions;
+pub mod prompt;
 #[cfg(feature = "completion")]
 pub mod completion;
 #[cfg(feature = "history")]
