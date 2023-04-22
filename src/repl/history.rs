0f863ced2590f6141444b132684555447eac1805
Nathan Lilienthal <nathan@nixpulvis.com>
2023-04-22 15:35:17 -0400

    Removed a HACK, thanks clippy.

--- src/repl/history.rs
+++ src/repl/history.rs
@@ -20,12 +20,12 @@ impl History {
             return;
         }
 
-        // HACK: There's got to be a cleaner way.
         let mut index = 0;
-        if self.1.iter().enumerate().find(|(i, (t, _))| {
-            index = *i;
-            text == t
-        }).is_some() {
+        if self.1.iter().enumerate()
+                        .any(|(_,(t, i))| {
+                            index = *i;
+                            text == t })
+        {
             self.1[index].1 += count;
             let text = self.1.remove(index);
             self.1.insert(0, text);
